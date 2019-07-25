8547c858415abcd59ff4ddfedb73d8e7e5dae419
Raphael Leardini <raphaelleardini@hotmail.com>
2019-07-25 15:49:16 -0300

    Instanciation and findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,13 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao(); //injecao de dependencia -> sem explicitar a implementacao
 		
+		System.out.println("=== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("=== TEST 2: List<seller> findByDepartment ====");
+		List<Seller> sellers = sellerDao.findByDepartment(new Department(2, null));
+		System.out.println(sellers);
 	}
 
 }
